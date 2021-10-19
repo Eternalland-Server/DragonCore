e06f8787b2924053ae5bdc13e65b68efe4307149
justwei <admin@taylorswiftcn.com>
2021-10-19 19:29:03 +0800

    [update] 数据库操作改为异步

--- src/main/java/eos/moe/dragoncore/api/SlotAPI.java
+++ src/main/java/eos/moe/dragoncore/api/SlotAPI.java
@@ -4,12 +4,15 @@ import eos.moe.dragoncore.DragonCore;
 import eos.moe.dragoncore.database.IDataBase;
 import eos.moe.dragoncore.network.PacketSender;
 import org.apache.commons.lang.Validate;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Map;
 
 public class SlotAPI {
+
+    private static DragonCore plugin = DragonCore.getInstance();
 
     public static void setSlotItem(Player player, String identifier, ItemStack itemStack, boolean syncToClient) {
         setSlotItem(player, identifier, itemStack, syncToClient, null);
@@ -19,7 +22,7 @@ public class SlotAPI {
         Validate.notNull(identifier, "identifier cant be null");
         Validate.notNull(itemStack, "itemStack cant be null");
 
-        DragonCore.getInstance().getDB().setData(player, identifier, itemStack, new IDataBase.Callback<ItemStack>() {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> plugin.getDB().setData(player, identifier, itemStack, new IDataBase.Callback<ItemStack>() {
             @Override
             public void onResult(ItemStack p0) {
                 DragonCore.getInstance().getMiscManager().putItem(player, identifier, itemStack);
@@ -37,23 +40,23 @@ public class SlotAPI {
                     callback.onFail();
                 }
             }
-        });
+        }));
     }
 
     public static void getSlotItem(Player player, String identifier, IDataBase.Callback<ItemStack> callback) {
-        DragonCore.getInstance().getDB().getData(player, identifier, callback);
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> plugin.getDB().getData(player, identifier, callback));
     }
 
     public static void getAllSlotItem(Player player, IDataBase.Callback<Map<String, ItemStack>> callback) {
-        DragonCore.getInstance().getDB().getAllData(player, callback);
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> plugin.getDB().getAllData(player, callback));
     }
 
     public static ItemStack getCacheSlotItem(Player player, String identifier) {
-        Map<String, ItemStack> map = DragonCore.getInstance().getMiscManager().getCacheMap().get(player.getUniqueId());
+        Map<String, ItemStack> map = plugin.getMiscManager().getCacheMap().get(player.getUniqueId());
         return map != null ? map.get(identifier) : null;
     }
 
     public static Map<String, ItemStack> getCacheAllSlotItem(Player player) {
-        return DragonCore.getInstance().getMiscManager().getCacheMap().get(player.getUniqueId());
+        return plugin.getMiscManager().getCacheMap().get(player.getUniqueId());
     }
 }
--- src/main/java/eos/moe/dragoncore/listener/misc/MiscManager.java
+++ src/main/java/eos/moe/dragoncore/listener/misc/MiscManager.java
@@ -21,12 +21,10 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MiscManager implements Listener {
     private Map<UUID, Map<String, ItemStack>> cacheMap;
     private DragonCore plugin;
-    private Set<Update> updateSet;
 
     public MiscManager(DragonCore plugin) {
         this.plugin = plugin;
         this.cacheMap = new ConcurrentHashMap<>();
-        this.updateSet = new HashSet<>();
 
         PluginManager pm = Bukkit.getPluginManager();
         YamlConfiguration config = Config.getConfig();
@@ -41,21 +39,19 @@ public class MiscManager implements Listener {
         Player player = e.getPlayer();
         cacheMap.remove(e.getPlayer().getUniqueId());
         System.out.println("[DragonCore] 开始载入玩家 " + player.getName() + " 物品");
-        plugin.getDB().getAllData(player, new IDataBase.Callback<Map<String, ItemStack>>() {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> plugin.getDB().getAllData(player, new IDataBase.Callback<Map<String, ItemStack>>() {
             @Override
             public void onResult(Map<String, ItemStack> p0) {
                 MiscManager.this.cacheMap.put(player.getUniqueId(), p0);
-                update1(player);
                 System.out.println("[DragonCore] 载入玩家 " + player.getName() + " 物品完成,共 " + p0.size() + " 个物品");
             }
 
             @Override
             public void onFail() {
                 MiscManager.this.cacheMap.put(player.getUniqueId(), new HashMap<>());
-                update1(player);
                 System.out.println("[DragonCore] 载入玩家 " + player.getName() + " 物品失败");
             }
-        });
+        }));
     }
 
 
@@ -67,29 +63,8 @@ public class MiscManager implements Listener {
     public void putItem(Player player, String identifier, ItemStack itemStack) {
         if (cacheMap.containsKey(player.getUniqueId())) {
             cacheMap.get(player.getUniqueId()).put(identifier, itemStack);
-            this.update(player, false);
             new PlayerSlotUpdateEvent(player, identifier, itemStack).callEvent();
 
-        }
-    }
-
-    private void update1(Player player) {
-        update(player, true);
-        Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            if (player.isOnline())
-                update(player, true);
-        }, 40);
-    }
-
-    public void update(Player player, boolean callEvent) {
-        Map<String, ItemStack> map = this.cacheMap.get(player.getUniqueId());
-        if (map != null) {
-            for (Update update : updateSet) {
-                update.update(player, map);
-            }
-            if (callEvent) {
-                new PlayerSlotUpdateEvent(player, null, null).callEvent();
-            }
         }
     }
 
@@ -97,8 +72,4 @@ public class MiscManager implements Listener {
         return cacheMap;
     }
 
-    @Deprecated
-    public Set<Update> getUpdateSet() {
-        return updateSet;
-    }
 }
