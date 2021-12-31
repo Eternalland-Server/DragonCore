855064ff593c97928850c50e4fee5fb50cd2b0e7
justwei <admin@taylorswiftcn.com>
2021-12-31 14:55:57 +0800

    [update] DragonArmourersHook

--- src/main/java/net/sakuragame/eternal/dragoncore/listener/misc/DragonArmourersHook.java
+++ src/main/java/net/sakuragame/eternal/dragoncore/listener/misc/DragonArmourersHook.java
@@ -6,12 +6,13 @@ import eos.moe.armourers.api.PlayerSkinUpdateEvent;
 import net.sakuragame.eternal.dragoncore.api.event.PlayerSlotUpdateEvent;
 import net.sakuragame.eternal.dragoncore.config.FileManager;
 import net.sakuragame.eternal.dragoncore.config.SlotSetting;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class DragonArmourersHook implements Listener {
@@ -22,34 +23,40 @@ public class DragonArmourersHook implements Listener {
     }
 
     @EventHandler
-    public void onUpdate(PlayerSlotUpdateEvent e) {
+    public void onSlotUpdate(PlayerSlotUpdateEvent e) {
         DragonAPI.updatePlayerSkin(e.getPlayer());
     }
 
     @EventHandler
-    public void updateSkin(PlayerSkinUpdateEvent e) {
+    public void onSkinUpdate(PlayerSkinUpdateEvent e) {
         Player player = e.getPlayer();
+        e.setSkinList(getSkins(player));
+    }
+
+    private List<String> getSkins(Player player) {
+        List<String> skins = new ArrayList<>();
 
         Map<String, ItemStack> map = manager.getCacheMap().get(player.getUniqueId());
         if (map == null) {
-            return;
+            return skins;
         }
 
-        for (String key : map.keySet()) {
+        for (String key : FileManager.getSlotSettings().keySet()) {
             ItemStack item = map.get(key);
 
             SlotSetting slotSetting = FileManager.getSlotSettings().get(key);
-            if (slotSetting == null) continue;
             if (!slotSetting.isSkin()) continue;
 
             if (!MegumiUtil.isEmpty(item)) {
-                e.getSkinList().add(DragonAPI.getItemSkinName(item));
+                skins.add(DragonAPI.getItemSkinName(item));
             }
             else {
                 if (slotSetting.getIdentifier().equals("offhand_skin")) {
-                    e.getSkinList().add("eternal-shield");
+                    skins.add("eternal-shield");
                 }
             }
         }
+
+        return skins;
     }
 }
