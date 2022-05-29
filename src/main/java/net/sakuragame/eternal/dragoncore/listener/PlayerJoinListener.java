863d0b677dd9432412165476aa18c3201548fde4
justwei <admin@taylorswiftcn.com>
2022-05-29 13:33:34 +0800

    🚧 update

--- src/main/java/net/sakuragame/eternal/dragoncore/listener/PlayerJoinListener.java
+++ src/main/java/net/sakuragame/eternal/dragoncore/listener/PlayerJoinListener.java
@@ -4,11 +4,15 @@ import net.sakuragame.eternal.dragoncore.config.ClientHandler;
 import net.sakuragame.eternal.dragoncore.config.sub.ConfigFile;
 import net.sakuragame.eternal.dragoncore.network.PacketSender;
 import net.sakuragame.eternal.dragoncore.util.Scheduler;
+import net.sakuragame.serversystems.manage.client.api.ClientManagerAPI;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PlayerJoinListener implements Listener {
 
@@ -18,6 +22,11 @@ public class PlayerJoinListener implements Listener {
         Scheduler.runLaterAsync(() -> {
             if (player == null) return;
             ClientHandler.sendYaml2Player(e.getPlayer());
+
+            int uid = ClientManagerAPI.getUserID(player.getUniqueId());
+            Map<String, String> map = new HashMap<>();
+            map.put("eternal_user_uid", uid + "");
+            PacketSender.sendSyncPlaceholder(player, map);
         }, ConfigFile.joinPacketDelay);
     }
 
